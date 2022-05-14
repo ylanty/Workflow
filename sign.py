311202c842a90f3c917175f0cb8e3d00a2ad11d9
ylanty <49444052+ylanty@users.noreply.github.com>
2022-05-14 10:42:02 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -11,24 +11,30 @@ token = session.post(url = url,data=data).json()['token']
 url = 'https://xiaoaishe.com/wp-json/jwt-auth/v1/token'
 data = {'username': os.environ['xiaoaishe_username'],'password': os.environ['xiaoaishe_password'],'token': token}
 login_cookies = session.post(url = url,data=data).cookies.get_dict()
-#login_cookies['b2_back_url']='https://xiaoaishe.com/vips'
-'''b2_token = login_cookies['b2_token']
-PHPSESSID = login_cookies['PHPSESSID']
-
-#获取PHPSESSID
-url = 'https://xiaoaishe.com/wp-json/b2/v1/tjuser'
-cookies = dict(b2_token=b2_token)
-requests.utils.add_dict_to_cookiejar(session.cookies, cookies)
-data = {}
-headers = {'authorization': 'Bearer '+b2_token}
-PHPSESSID = session.post(url = url,data=data, headers=headers).cookies.get_dict()['PHPSESSID']
-#print(PHPSESSID)'''
 #签到
 url = 'https://xiaoaishe.com/wp-json/b2/v1/userMission'
 #cookies = dict(b2_back_url='https://xiaoaishe.com/vips',b2_token=b2_token,PHPSESSID=PHPSESSID)
-
-requests.utils.add_dict_to_cookiejar(session.cookies, login_cookies)
+#requests.utils.add_dict_to_cookiejar(session.cookies, login_cookies)
 data = {}
 headers = {'authorization': 'Bearer '+login_cookies['b2_token']}
+jg = session.post(url=url, data=data, headers=headers)
+print(jg.text)
+
+session = requests.session()
+#登录
+url = 'https://maozhua.org/wp-json/jwt-auth/v1/token'
+data = {'username': os.environ['maozhua_username'],'password': os.environ['maozhua_password']}
+login_cookies = session.post(url = url,data=data).cookies.get_dict()
+#print(login_cookies)
+#获取PHPSESSID
+url = 'https://maozhua.org/wp-json/b2/v1/tjuser'
+data = {}
+headers = {'authorization': 'Bearer '+login_cookies['b2_token']}
+cookies = session.post(url = url,data=data, headers=headers).cookies.get_dict()
+#print(cookies['PHPSESSID'])
+#maozhua签到
+url = 'https://maozhua.org/wp-json/b2/v1/userMission'
+data = {}
+headers = {'authorization': 'Bearer '+b2_token}
 jg = session.post(url=url, data=data, headers=headers)
 print(jg.text)
