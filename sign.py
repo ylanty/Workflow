44cfdf267984fb6f4c1fdbe0eeb92f4488442ee1
ylanty <49444052+ylanty@users.noreply.github.com>
2022-05-20 07:40:39 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -3,13 +3,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def xiaoaishe_sign():
-    # 找到插件的路径，使用它驱动操作
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    browser = webdriver.Chrome(options=chrome_options)
-    #browser = webdriver.Chrome('C:\Program Files\Google\Chrome\Application\chromedriver.exe')
-     
+def xiaoaishe_sign(browser):
     # 选择需要打卡的网址，填入你的签到网页
     browser.get('https://xiaoaishe.com/mission/today')
     browser.find_element_by_xpath('//div[@class="modal-content b2-radius"]//span[@class="close-button"]').click()
@@ -19,7 +13,6 @@ def xiaoaishe_sign():
     # 找到邮件和密码输入框的xpath,并在对应的位置送入账号密码
     browser.find_element_by_xpath('//label[@class="login-form-item"]//input[@name="username"]').send_keys(os.environ["xiaoaishe_username"])
     browser.find_element_by_xpath('//label[@class="login-form-item"]//input[@name="password"]').send_keys(os.environ["xiaoaishe_password"])
-     
     # 找到登录按钮的xpath，模拟点击
     browser.find_element_by_xpath('//div[@class="login-box-in"]//div[@class="login-bottom"]//button').click()
     time.sleep(2)
@@ -27,15 +20,8 @@ def xiaoaishe_sign():
     browser.find_element_by_xpath('//div[@class="bar-item bar-mission"]').click()
     time.sleep(2)
     browser.find_element_by_xpath('//div[@class="bar-user-info-row bar-mission-action"]').click()
-    browser.quit()
     
-def maozhua_sign():
-    # 找到插件的路径，使用它驱动操作
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    browser = webdriver.Chrome(options=chrome_options)
-    #browser = webdriver.Chrome('C:\Program Files\Google\Chrome\Application\chromedriver.exe')
-     
+def maozhua_sign(browser):
     # 选择需要打卡的网址，填入你的签到网页
     browser.get('https://maozhua.org/mission/today')
     browser.find_element_by_xpath('//button[@class="empty mobile-hidden"]').click()
@@ -43,7 +29,6 @@ def maozhua_sign():
     # 找到邮件和密码输入框的xpath,并在对应的位置送入账号密码
     browser.find_element_by_xpath('//label[@class="login-form-item"]//input[@name="username"]').send_keys(os.environ["maozhua_username"])
     browser.find_element_by_xpath('//label[@class="login-form-item"]//input[@name="password"]').send_keys(os.environ["maozhua_password"])
-     
     # 找到登录按钮的xpath，模拟点击
     browser.find_element_by_xpath('//div[@class="login-box-in"]//div[@class="login-bottom"]//button').click()
     time.sleep(2)
@@ -51,8 +36,12 @@ def maozhua_sign():
     browser.find_element_by_xpath('//div[@class="bar-item bar-mission"]').click()
     time.sleep(2)
     browser.find_element_by_xpath('//div[@class="bar-user-info-row bar-mission-action"]').click()
-    browser.quit()
  
 if __name__ == '__main__':
-    xiaoaishe_sign()
-    maozhua_sign()
+    # 找到插件的路径，使用它驱动操作
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    browser = webdriver.Chrome(options=chrome_options)
+    xiaoaishe_sign(browser)
+    maozhua_sign(browser)
+    browser.quit()
