de07a4e386185ceb1b89493441ef7bcf088ae110
ylanty <49444052+ylanty@users.noreply.github.com>
2022-05-25 10:59:22 +0800

    Update func.py

--- func.py
+++ func.py
@@ -2,9 +2,9 @@ import time
 
 def daylylog(msg):
     try:
-        #file_handle=open('daylylog.txt',mode='a')
-        #file_handle.writelines([time.strftime("%Y/%m/%d"),msg,'\n'])
-        #file_handle.close()
+        file_handle=open('daylylog.txt',mode='a')
+        file_handle.writelines([time.strftime("%Y/%m/%d"),msg,'\n'])
+        file_handle.close()
         print(time.strftime("%Y/%m/%d")+" "+msg)
     except Exception as e:
         print("daylylog有错误:", e)
