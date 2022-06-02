06c9ce0e21af14ae87cfeaee252e27e88b3f845b
ylanty <49444052+ylanty@users.noreply.github.com>
2022-06-02 22:10:44 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,9 +1,13 @@
+import pymysql
 import time
 import os
 import func
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+
+xiaoaishe_res = ''
+maozhua_res = ''
 
 def xiaoaishe_sign(browser):
     try:
@@ -26,9 +30,17 @@ def xiaoaishe_sign(browser):
         time.sleep(2)
         browser.find_element(By.XPATH,'//div[@class="bar-user-info-row bar-mission-action"]').click()
         time.sleep(2)
-        func.daylylog(browser.find_element(By.XPATH,'//div[@class="bar-user-info-row bar-mission-action"]').text)
+        # func.daylylog(browser.find_element(By.XPATH,'//div[@class="bar-user-info-row bar-mission-action"]').text)
+        global xiaoaishe_res
+        xiaoaishe_res = browser.find_element(By.XPATH,'//div[@class="bar-user-info-row bar-mission-action"]').text
+        if '恭喜' in xiaoaishe_res :
+            print(xiaoaishe_res)
+            return True
+        else:
+            return False
     except Exception as e:
         print("xiaoaishe有错误:", e)
+        return False
     
 def maozhua_sign(browser):
     try:
@@ -48,9 +60,17 @@ def maozhua_sign(browser):
         time.sleep(2)
         browser.find_element(By.XPATH,'//div[@class="bar-user-info-row bar-mission-action"]').click()
         time.sleep(2)
-        func.daylylog(browser.find_element(By.XPATH,'//div[@class="bar-user-info-row bar-mission-action"]').text)
+        # func.daylylog(browser.find_element(By.XPATH,'//div[@class="bar-user-info-row bar-mission-action"]').text)
+        global maozhua_res
+        maozhua_res = browser.find_element(By.XPATH,'//div[@class="bar-user-info-row bar-mission-action"]').text
+        if '恭喜' in maozhua_res :
+            print(maozhua_res)
+            return True
+        else:
+            return False
     except Exception as e:
         print("maozhua有错误:", e)
+        return False
  
 def sdai_sign(browser):
     try:
@@ -76,14 +96,73 @@ def sdai_sign(browser):
         func.daylylog(browser.find_element(By.XPATH,'//fieldset/div[4]').text)
     except Exception as e:
         print("sdai有错误:", e)
-    
+
+def executesql(web_name,result):
+    # 打开数据库连接
+    db = pymysql.connect(host='os.environ["mysql_host"]',
+                         user='os.environ["mysql_username"]',
+                         password='os.environ["mysql_password"]',
+                         database='os.environ["mysql_database"]')
+    # 使用cursor()方法获取操作游标 
+    cursor = db.cursor()
+    # SQL 语句
+    sql = "INSERT INTO `daylylog`(`web_name`, `day_time`, `result`, `sys_time`) VALUES ('%s', utc_date(), '%s',sysdate())" %(web_name,result)
+    try:
+       # 执行sql语句
+       cursor.execute(sql)
+       # 执行sql语句
+       db.commit()
+    except:
+       # 发生错误时回滚
+       db.rollback()
+    finally:
+        # 关闭数据库连接
+        cursor.close()
+        db.close()
+
+def isexecuted(web_name):
+    # 打开数据库连接
+    db = pymysql.connect(host='os.environ["mysql_host"]',
+                         user='os.environ["mysql_username"]',
+                         password='os.environ["mysql_password"]',
+                         database='os.environ["mysql_database"]')
+    # 使用cursor()方法获取操作游标 
+    cursor = db.cursor()
+    # SQL 语句
+    sql = "SELECT * FROM `daylylog` WHERE `web_name` = '%s' and `day_time` = utc_date() " %web_name
+    try:
+       # 执行sql语句
+       cursor.execute(sql)
+       # 获取所有记录列表
+       results = cursor.fetchall()
+       if len(results)>0:
+           return True
+       else:
+           return False
+    except:
+       # 发生错误
+       print ("Error: unable to fetch data")
+       return False
+    finally:
+        # 关闭数据库连接
+        cursor.close()
+        db.close()
+
 if __name__ == '__main__':
-    # 找到插件的路径，使用它驱动操作
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('ignore-certificate-errors')
     browser = webdriver.Chrome(options=chrome_options)
-    xiaoaishe_sign(browser)
-    maozhua_sign(browser)
+    # 找到插件的路径，使用它驱动操作
+    if isexecuted('xiaoaishe') :
+        print('xiaoaishe已签到')
+    elif xiaoaishe_sign(browser) :
+        executesql('xiaoaishe',xiaoaishe_res)
+    if isexecuted('maozhua') :
+        print('maozhua已签到')
+    elif maozhua_sign(browser) :
+        executesql('maozhua',maozhua_res)
+    # xiaoaishe_sign(browser)
+    # maozhua_sign(browser)
     # sdai_sign(browser)
     browser.quit()
