da5dfdeff56ab1fe52032ad9c6720992e8a204fe
ylanty <49444052+ylanty@users.noreply.github.com>
2022-06-02 22:16:30 +0800

    Update func.py

--- func.py
+++ func.py
@@ -1,5 +1,57 @@
+import pymysql
 import time
 
+def executesql(web_name,result):
+    # 打开数据库连接
+    db = pymysql.connect(host='os.environ["mysql_host"]',
+                         user='os.environ["mysql_username"]',
+                         password='os.environ["mysql_password"]',
+                         database='os.environ["mysql_database"]')
+    # 使用cursor()方法获取操作游标 
+    cursor = db.cursor()
+    # SQL 语句
+    sql = "INSERT INTO `daylylog`(`web_name`, `day_time`, `result`, `sys_time`) VALUES ('%s', utc_date(), '%s',sysdate())" %(web_name,result)
+    try:
+       # 执行sql语句
+       cursor.execute(sql)
+       # 执行sql语句
+       db.commit()
+    except:
+       # 发生错误时回滚
+       db.rollback()
+    finally:
+        # 关闭数据库连接
+        cursor.close()
+        db.close()
+
+def isexecuted(web_name):
+    # 打开数据库连接
+    db = pymysql.connect(host='os.environ["mysql_host"]',
+                         user='os.environ["mysql_username"]',
+                         password='os.environ["mysql_password"]',
+                         database='os.environ["mysql_database"]')
+    # 使用cursor()方法获取操作游标 
+    cursor = db.cursor()
+    # SQL 语句
+    sql = "SELECT * FROM `daylylog` WHERE `web_name` = '%s' and `day_time` = utc_date() " %web_name
+    try:
+       # 执行sql语句
+       cursor.execute(sql)
+       # 获取所有记录列表
+       results = cursor.fetchall()
+       if len(results)>0:
+           return True
+       else:
+           return False
+    except:
+       # 发生错误
+       print ("Error: unable to fetch data")
+       return False
+    finally:
+        # 关闭数据库连接
+        cursor.close()
+        db.close()
+        
 def daylylog(msg):
     try:
         file_handle=open('daylylog.txt',mode='a')
